9b21e2b244adb65e7fab0564f1beac13dc02d33b
Macsim6969 <macs.belousov666@gmail.com>
2022-02-10 22:27:29 +0300

    0210 up classcomp

--- src/components/Find friends/FIndfriends.jsx
+++ src/components/Find friends/FIndfriends.jsx
@@ -4,39 +4,41 @@ import {setFriends} from "../../Redux/Friend-reducer";
 import * as axios from "axios";
 import usersPhoto from '../../assets/image/users.png';
 
-const Findfriends = (props) => {
-    let getUsers = () =>{
-        if (props.users.length === 0){
 
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response =>{
-    
-                props.setFr( response.data.items)
-            });
-    
-    
-        }
+class Findfriends extends React.Component {
+
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setFr(response.data.items)
+        });
     }
 
-    return (
-        <div className={ss.log_find}>
-            People You May Know
-            <button onClick={getUsers}>Show Me</button>
-            <div className={ss.find}>{props.users.map(u =>
-                <div className={ss.findfriend} key={u.id}>
-                    <div className={ss.friendsss}>
-                        <nav><img className={ss.img} src={u.photos.small != null ? u.photos.small : usersPhoto} /></nav>
-                        <span className={ss.friendbiogr}>
+    render() {
+        return (
+            <div className={ss.log_find}>
+                People You May Know
+                <div className={ss.find}>{this.props.users.map(u =>
+                    <div className={ss.findfriend} key={u.id}>
+                        <div className={ss.friendsss}>
+                            <nav><img className={ss.img} src={u.photos.small != null ? u.photos.small : usersPhoto}/>
+                            </nav>
+                            <span className={ss.friendbiogr}>
                             <div>{u.name}</div>
                             <div>{'u.friend'}</div>
                         </span>
-                    </div>
-                    <div>
-                        {u.add ? <button onClick={ ()=> { props.delete(u.id)}}>Add friend</button > : <button onClick={ ()=> { props.add(u.id)}}>Remove</button>}
-                    </div>
-                </div>)}
+                        </div>
+                        <div>
+                            {u.add ? <button onClick={() => {
+                                this.props.delete(u.id)
+                            }}>Add friend</button> : <button onClick={() => {
+                                this.props.add(u.id)
+                            }}>Remove</button>}
+                        </div>
+                    </div>)}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Findfriends;
